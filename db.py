4d07f3eddc61466dec1b6a497ffd0b776a04f489
blank6890 <hiteshreddy68900@gmail.com>
2026-03-18 17:05:34 +0530

    fix SSL and MongoDB network access

--- db.py
+++ db.py
@@ -1,19 +1,20 @@
 from pymongo import MongoClient
 from urllib.parse import quote_plus
 import os
+import certifi
 
 username = "mediqueue-admin"
 password = quote_plus(os.environ.get("MONGO_PASSWORD", "Mediqueue@123"))
 cluster = "cluster0.bxatiz8.mongodb.net"
 
-MONGO_URI = f"mongodb+srv://{username}:{password}@{cluster}/?appName=Cluster0"
+MONGO_URI = f"mongodb+srv://{username}:{password}@{cluster}/?appName=Cluster0&tlsCAFile={certifi.where()}"
 
 client = None
 db = None
 
 def init_db():
     global client, db
-    client = MongoClient(MONGO_URI)
+    client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
     db = client["mediqueue"]
     print("Connected to MongoDB Atlas successfully!")
 
